ff8358ed50adda425c93d29726f4b81ffb573703
duducritico <160251842+duducritico@users.noreply.github.com>
2025-06-26 11:16:04 -0300

    alguma coisa

---
+++ script.js
@@ -0,0 +1,12 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const perguntas = [ // Arry com todas as perguntas do quiz
+    {
+        enunciado: "Você esta se preparando para pegar a primeira curva. Qual a sua estratégia?",
+    
+    }]
+
+
